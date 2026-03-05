e6b27033895c6f6c1f0c9563b47be1fa30f1da61
Lindokuhle Khumalo <klindokuhle1@gmail.com>
2026-03-05 15:26:22 +0200

    Initial commit

---
+++ WeatherApplication/WeatherApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApplicationApp.swift
+//  WeatherApplication
+//
+//  Created by Nhlanhla Kubayi on 2026/03/05.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
